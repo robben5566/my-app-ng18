0c1893b70989c10938e8135d54159a2a79aaf513
TB <tfy5566@gmail.com>
2024-09-27 01:46:11 +0800

    back to 7d87e4b

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,49 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  let vi = 'AĂÂBCDĐEÊGHIKLMNOÔƠPQRSTUƯVXYaăâbcdđeêghiklmnoôơpqrstuưvxy'
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
   });
 
-  // it('should create the app', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
-  // it(`should have the 'my-app-ng18' title`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('my-app-ng18');
-  // });
-
-  it('[pass] vi', () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.autoDetectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-
-    const inputEle = compiled.querySelector('input');
-    inputEle!.value = vi;
-    inputEle!.dispatchEvent(new Event('input'));
-
-    expect(compiled.querySelector('h2')?.textContent).toContain(vi);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 
-  it('[fail] ❤️', () => {
+  it(`should have the 'my-app-ng18' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.autoDetectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-
-    const inputEle = compiled.querySelector('input');
-    inputEle!.value = '❤️';
-    inputEle!.dispatchEvent(new Event('input'));
-
-    expect(compiled.querySelector('p')?.textContent).toContain('Error');
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('my-app-ng18');
   });
 
-
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-app-ng18');
+  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormGroup, FormControl, Validator, Validators, ValidatorFn, AbstractControl, ValidationErrors, ReactiveFormsModule } from '@angular/forms';
-import emojiRegex from 'emoji-regex';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'my-app-ng18';
-
-  accountCheck: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-    if (!this.form) {
-      return null;
-    }
-    const regex = emojiRegex();
-    console.log(control.value?.match(regex))
-
-    return control.value?.match(regex) ? { message: 'fuck' } : null;
-
-  };
-
-  form = new FormGroup({
-    account: new FormControl('', [Validators.required, this.accountCheck])
-  })
-
-  ngOnInit(): void {
-
-  }
-
-
 }
